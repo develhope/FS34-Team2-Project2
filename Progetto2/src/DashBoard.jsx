95d741cba81e23a9e04e2602aca7a6180d00908a
DavideTosti <davidetosti2002@gmail.com>
2025-05-05 13:09:51 +0200

    styling grafici dashboard

--- Progetto2/src/DashBoard.jsx
+++ Progetto2/src/DashBoard.jsx
@@ -4,16 +4,16 @@ import SpiralTimelineChart from "./Components/SpiralTimelineChart";
 
 export default function DashBoard() {
   return (
-    <>
+    <div>
       <div className="dashBoard-all">
         <div className="dashBoard">
+          <NavBarDash></NavBarDash>
           <div className="grafici">
-            <NavBarDash></NavBarDash>
             <Chart></Chart>
             <SpiralTimelineChart></SpiralTimelineChart>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
